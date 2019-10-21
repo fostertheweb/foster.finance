efaaee91bcb948d7e64ef56cbd9932f9c1917e34
Jonathan Foster <jxf9099@rit.edu>
2019-10-21 12:37:32 -0400

    Update Logo.js

--- web/src/common/Logo.js
+++ web/src/common/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function() {
   return (
     <div className="logo">
-      <div className="icon"></div>
+      <div className="icon">💸</div>
       <div className="text">
         <div className="top">Foster</div>
         <div className="bottom">Finance</div>
