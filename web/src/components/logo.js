1ee2b26f59661f841f3db709c3ca885eeef40b60
Jonathan Foster <jxf9099@rit.edu>
2019-11-11 21:48:05 -0500

    Update logo.js

--- web/src/components/logo.js
+++ web/src/components/logo.js
@@ -4,7 +4,7 @@ import Emoji from "./emoji";
 export default function() {
   return (
     <div className="logo">
-      <Emoji char={"�"} label="money with wings" className="icon" />
+      <Emoji char={"💸"} label="money with wings" className="icon" />
       <div className="text">
         <div className="top">Foster</div>
         <div className="bottom">Finance</div>
