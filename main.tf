e0c18986b9be62aa6e67c0307577f82c2cba80e7
Jonathan Foster <jxf9099@rit.edu>
2019-12-14 10:45:19 -0500

    clean up copy pasta

--- main.tf
+++ main.tf
@@ -136,13 +136,13 @@ resource "aws_s3_bucket_object" "client" {
 
 data "aws_iam_policy_document" "s3_policy" {
   statement {
-    sid       = "PublicRead"
+    sid       = "1"
     effect    = "Allow"
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.client.arn}/*"]
 
     principals {
-      type        = "CanonicalUser"
+      type        = "AWS"
       identifiers = [aws_cloudfront_origin_access_identity.access_identity.iam_arn]
     }
   }
@@ -240,14 +240,14 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.selected.id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.selected.id
+  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
