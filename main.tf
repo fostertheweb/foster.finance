fc254975ff57b972f0f885f76a551d843c2fe370
Jonathan Foster <jxf9099@rit.edu>
2019-12-23 12:57:32 -0500

    deprecated

--- main.tf
+++ main.tf
@@ -154,8 +154,8 @@ data "aws_iam_policy_document" "s3_policy" {
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = "${aws_s3_bucket.client.id}"
-  policy = "${data.aws_iam_policy_document.s3_policy.json}"
+  bucket = aws_s3_bucket.client.id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
 
 #Cloudfront
