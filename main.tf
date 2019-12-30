5a389b4250701db937c96f454725bc3c6b54cdc6
Jonathan Foster <jxf9099@rit.edu>
2019-12-30 03:36:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,8 +45,11 @@ resource "aws_cognito_user_pool" "pool" {
 }
 
 resource "aws_cognito_user_pool_client" "client" {
-  name         = "${var.application}-client"
-  user_pool_id = aws_cognito_user_pool.pool.id
+  name                 = "${var.application}-client"
+  user_pool_id         = aws_cognito_user_pool.pool.id
+  callback_urls        = ["${var.domain_name}/signup/profile"]
+  default_redirect_uri = "${var.domain_name}/signup/profile"
+  explicit_auth_flows  = ["USER_PASSWORD_AUTH"]
 }
 
 resource "aws_cognito_user_pool_domain" "main" {
