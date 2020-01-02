7fe3d80038e4d8383d68d243f57ddbf840810ad3
Jonathan Foster <jxf9099@rit.edu>
2020-01-01 22:26:53 -0500

    😐

--- web/src/components/button.js
+++ web/src/components/button.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinnerThird } from "@fortawesome/pro-duotone-svg-icons";
 
