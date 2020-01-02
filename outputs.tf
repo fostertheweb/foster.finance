a8cec2cf8a1742ff37ed30f7b2e6eca102af2b25
Jonathan Foster <jxf9099@rit.edu>
2020-01-01 22:39:11 -0500

    add output for future build enhancement

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "client_files" {
+output "web_build_files" {
   value = fileset("./web/build", "**")
+}
+
+output "user_pool_id" {
+  value = aws_cognito_user_pool.users.id
+}
+
+output "client_id" {
+  value = aws_cognito_user_pool_client.web.id
 }
