f35a79b4a1b27f7b57b782cbdd0b18bac27b6ed7
Jonathan Foster <jxf9099@rit.edu>
2020-01-03 19:49:05 -0500

    maybe

--- email/index.js
+++ email/index.js
@@ -1,12 +1,19 @@
+const crypto = require("crypto");
+
 function handler(event, _context, callback) {
   const code = event.request.codeParameter;
   const email = event.request.usernameParameter;
+  const hash = crypto.createHash("sha256");
+
+  hash.update(`${email}+${code}`);
 
   switch (event.triggerSource) {
     case "CustomMessage_SignUp":
       event.response.emailSubject = "is it you?";
       event.response.emailMessage = `
-      Hello, click this link <a href="https://foster.finance/signup/verify?code=${code}&email=${email}">Verify Email</a>
+Hello, click this link <a href="https://foster.finance/signup/verify?token=${hash.digest(
+        "hex",
+      )}">Verify Email</a>
       `;
       break;
     default:
