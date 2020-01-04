ddbba96c260987e4ff9d631afbc3c248c144a40a
Jonathan Foster <jxf9099@rit.edu>
2020-01-03 19:30:48 -0500

    signup email

--- email/index.js
+++ email/index.js
@@ -1,5 +1,21 @@
-function handler(event, context, callback) {
-  console.log({ event, context, callback });
+function handler(event, _context, callback) {
+  const code = event.request.codeParameter;
+  const email = event.request.usernameParameter;
+
+  switch (event.triggerSource) {
+    case "CustomMessage_SignUp":
+      event.response.emailSubject = "is it you?";
+      event.response.emailMessage = `
+      Hello, click this link <a href="https://foster.finance/signup/verify?code=${code}&email=${email}">Verify Email</a>
+      `;
+      break;
+    default:
+      event.response.emailSubject = "poggers";
+      event.response.emailMessage = "pog";
+      break;
+  }
+
+  callback(null, event);
 }
 
 module.exports = { handler };
