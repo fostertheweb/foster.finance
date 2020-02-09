0c0fa20bf42bb38a6f73ea3ecbe6eeba938100a8
Jonathan Foster <jxf9099@rit.edu>
2020-02-09 13:00:42 -0500

    couch potato

--- server/src/handler.js
+++ server/src/handler.js
@@ -18,14 +18,7 @@ async function graphql(event, context) {
     schema,
     context: buildContext(db),
   });
-  const handler = server.createHandler({
-    cors: {
-      origin: true,
-      credentials: true,
-      methods: "POST",
-      allowedHeaders: ["Content-Type", "Origin", "Accept"],
-    },
-  });
+  const handler = server.createHandler();
   const response = await run(event, context, handler);
 
   return response;
