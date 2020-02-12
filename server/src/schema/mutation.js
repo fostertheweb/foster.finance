d3f7c6da65fd4cf86b01cf6de57a12f75a29fcde
Jonathan Foster <jxf9099@rit.edu>
2020-02-12 16:36:34 -0500

    Update mutation.js

--- server/src/schema/mutation.js
+++ server/src/schema/mutation.js
@@ -1,6 +1,7 @@
 const { arg, objectType, stringArg } = require("nexus");
 const { User, CreateUserRequest } = require("./user");
 const { ExchangePublicTokenResponse } = require("./plaid");
+const database = require("../database");
 
 const Mutation = objectType({
   name: "Mutation",
@@ -13,8 +14,9 @@ const Mutation = objectType({
           required: true,
         }),
       },
-      async resolve(_root, { input }, { db }) {
+      async resolve(_root, { input }) {
         console.log({ mutation: "createUser", input });
+        const db = await database;
         return await db.saveDoc("users", input);
       },
     });
