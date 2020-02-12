ab4cbdd069ab7aa41887a95646e1bc9bc2279c02
Jonathan Foster <jxf9099@rit.edu>
2020-02-12 16:47:19 -0500

    Update handler.js

--- server/src/handler.js
+++ server/src/handler.js
@@ -1,19 +1,31 @@
 const { ApolloServer } = require("apollo-server-lambda");
 const { schema, plaid } = require("./app");
-const database = require("./database");
 
 const server = new ApolloServer({
   schema,
   context: () => ({ plaid }),
 });
 
-const graphql = server.createHandler({
-  cors: {
-    origin: true,
-    credentials: true,
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Origin", "Accept", "Authorization"],
-  },
-});
+function run(event, context, handler) {
+  return new Promise((resolve, reject) => {
+    function callback(error, body) {
+      return error ? reject(error) : resolve(body);
+    }
+    
+    handler(event, context, callback);
+  });
+}
 
-module.exports = { graphql };
+export async function graphql(event, context) {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const handler = server.createHandler({
+    cors: {
+      origin: true,
+      credentials: true,
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Origin", "Accept", "Authorization"],
+    },
+  });
+
+  return await run(event, context, handler);
+}
