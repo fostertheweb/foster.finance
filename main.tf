76f472b9302e6ba134bb5362e8a0dc33177b6c84
Jonathan Foster <jxf9099@rit.edu>
2020-02-14 15:07:51 -0500

    will it go away?

--- main.tf
+++ main.tf
@@ -21,17 +21,6 @@ locals {
   }
 }
 
-resource "aws_db_instance" "users_db" {
-  allocated_storage = 20
-  storage_type      = "gp2"
-  engine            = "postgres"
-  engine_version    = "11.5"
-  instance_class    = "db.t2.micro"
-  name              = var.db_name
-  username          = var.db_user
-  password          = var.db_pass
-  skip_final_snapshot = true
-}
 
 # Cognito
 resource "aws_cognito_user_pool" "users" {
