30a9f526af7883614fef1348bd89285092d8cd99
Jonathan Foster <jxf9099@rit.edu>
2020-02-17 15:43:34 -0500

    idk

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const app = require("fastify")({ logger: true });
 
 app.get("/ping", (_, reply) => reply.send("PONG"));
 
+app.register(require("./plugins/database"));
+app.register(require("./plugins/plaid"));
 app.register(require("fastify-cors"), {
   origin: ["https://foster.finance", "http://localhost:3000"],
   methods: ["GET", "POST", "PATCH"],
@@ -15,9 +17,6 @@ app.register(require("fastify-cors"), {
   credentials: true,
   maxAage: 300,
 });
-
-app.register(require("./plugins/database"));
-app.register(require("./plugins/plaid"));
 
 app.register(require("./routes/users"), { prefix: "/users" });
 app.register(require("./routes/plaid"), { prefix: "/plaid" });
