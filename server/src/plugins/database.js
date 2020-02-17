1623d261795524a660d5f2ddff5c245a1dc83e3f
Jonathan Foster <jxf9099@rit.edu>
2020-02-17 10:52:14 -0500

    move it

--- server/src/plugins/database.js
+++ server/src/plugins/database.js
@@ -1,6 +1,6 @@
 const fp = require("fastify-plugin");
 const admin = require("firebase-admin");
-const serviceAccount = require("../../service-account.json");
+const serviceAccount = require("../service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
