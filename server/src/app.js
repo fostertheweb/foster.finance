b759c6846e5a4ced3f58afe7b9bb019398d883ed
Jonathan Foster <jxf9099@rit.edu>
2020-02-16 20:08:30 -0500

    ping

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const app = require("fastify")({ logger: true });
 
+app.get("/ping", (_, reply) => reply.send("PONG"));
+
 app.register(require("./plugins/database"));
 app.register(require("./plugins/plaid"));
 
