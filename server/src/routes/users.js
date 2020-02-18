f8f40c2d6e394dec660c23146805483f858de31c
Jonathan Foster <jxf9099@rit.edu>
2020-02-17 19:48:18 -0500

    logs

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,12 +1,15 @@
 module.exports = function(fastify, _opts, done) {
   fastify.get("/:id", async (request, reply) => {
+    fastify.log.info("in the route");
     try {
+      fastify.log.info("fetching user");
       const doc = await fastify
         .db()
         .collection("users")
         .doc(request.params.id)
         .get();
       const user = doc.data();
+      fastify.log.info(user);
       return reply.send(user);
     } catch (err) {
       fastify.log.error(err);
