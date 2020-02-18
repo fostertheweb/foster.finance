f60ffb3053b44e9f72dc88d774bb4df9cfc26283
Jonathan Foster <jxf9099@rit.edu>
2020-02-18 16:36:30 -0500

    Update handler.js

--- server/src/handler.js
+++ server/src/handler.js
@@ -2,4 +2,4 @@ const lambda = require("aws-lambda-fastify");
 const app = require("./app");
 const proxy = lambda(app);
 
-module.exports.handler = proxy;
+module.exports.handler = async (event, context) => proxy(event, context);
