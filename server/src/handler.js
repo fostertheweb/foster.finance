345c4f89cd77b621361a068791168698315fe0e4
Jonathan Foster <jxf9099@rit.edu>
2020-02-17 19:58:43 -0500

    whatever

--- server/src/handler.js
+++ server/src/handler.js
@@ -1,14 +1,10 @@
-// const lambda = require("aws-lambda-fastify");
-const serverless = require("serverless-http");
+const lambda = require("aws-lambda-fastify");
 const app = require("./app");
-const proxy = serverless(app);
+const proxy = lambda(app);
 
 module.exports.handler = async (event, context) => {
-  // you can do other things here
   try {
     const result = await proxy(event, context);
-
-    // and here
     return result;
   } catch (err) {
     console.log(err);
