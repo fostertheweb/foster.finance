6a872b8618c319768b08fd6a442e5b48e52f9dba
Jonathan Foster <jxf9099@rit.edu>
2020-02-18 16:19:41 -0500

    Update handler.js

--- server/src/handler.js
+++ server/src/handler.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const proxy = lambda(app);
 
 module.exports.handler = async (event, context) => {
+  console.log("Handling the event now");
   context.callbackWaitsForEmptyEventLoop = false;
   return await proxy(event, context);
 };
