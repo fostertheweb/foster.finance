5aab1904dd6995393b242dc2ee1726a2fc52983d
Jonathan Foster <jxf9099@rit.edu>
2020-02-18 16:23:33 -0500

    Update users.js

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,6 +1,9 @@
 module.exports = function(fastify, _opts, next) {
   fastify.get("/:id", async function(request) {
+    fastify.log.info(`GET /users/${request.params.id}`);
     try {
+      fastify.log.info("Fetching user");
+      console.log(this.mongo);
       return await this.mongo.db.collection("users").findOne({ user_id: request.params.id });
     } catch (err) {
       fastify.log.error(err);
