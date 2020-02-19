82386f94fc88932c534a954f3ed78a078a4e84a2
Jonathan Foster <jxf9099@rit.edu>
2020-02-18 22:54:24 -0500

    oooooooooooooooooooo

--- server/src/handler.js
+++ server/src/handler.js
@@ -1,10 +1,10 @@
 const lambda = require("aws-lambda-fastify");
 const app = require("./app");
-const proxy = lambda(app);
+const handler = lambda(app);
 
 process.on("unhandledRejection", (reason, promise) => {
   console.log("Unhandled Rejection at:", promise, "reason:", reason);
   throw new Error(reason);
 });
 
-module.exports.handler = async (event, context) => proxy(event, context);
+module.exports = { handler };
