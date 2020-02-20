9a9f5dbfad3528722b04e7aa710b23f85aaa4da5
Jonathan Foster <jxf9099@rit.edu>
2020-02-19 22:13:55 -0500

    okay

--- server/src/handler.js
+++ server/src/handler.js
@@ -1,18 +1,44 @@
-const lambda = require("aws-lambda-fastify");
-const init = require("./app");
-const database = require("./database");
+// const lambda = require("aws-lambda-fastify");
+// const init = require("./app");
+// const database = require("./database");
 
-async function handler(event, context) {
+// async function handler(event, context) {
+//   context.callbackWaitsForEmptyEventLoop = false;
+//   try {
+//     const client = await database();
+//     const app = init(client);
+//     const proxy = lambda(app);
+//     return proxy(event, context);
+//   } catch (err) {
+//     console.log(err);
+//     throw err;
+//   }
+// }
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.DB_URL, {
+  useNewUrlParser: true,
+});
+
+let db;
+
+const createConnection = async () => {
+  await client.connect();
+  db = client.db("test");
+};
+
+module.exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   try {
-    const client = await database();
-    const app = init(client);
-    const proxy = lambda(app);
-    return proxy(event, context);
+    if (db) {
+      return await db.collection("users").find({});
+    } else {
+      await createConnection();
+      return await db.collection("users").find({});
+    }
   } catch (err) {
     console.log(err);
-    throw err;
   }
-}
+};
 
-module.exports = { handler };
+// module.exports = { handler };
