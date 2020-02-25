0e3d53237c6ae93a989925c0248fe678dc1dcd9a
Jonathan Foster <jxf9099@rit.edu>
2020-02-25 12:47:42 -0500

    quick clean

--- web/src/components/accounts/list.js
+++ web/src/components/accounts/list.js
@@ -5,17 +5,21 @@ export default function({ data }) {
     <div className="p-4">
       <h2>Accounts</h2>
       {data.map(account => (
-        <div key={account.account_id} class="flex justify-between items-center mt-2 p-4 bg-white hover:border-blue-500 rounded border-2 border-gray-300 cursor-pointer">
+        <div
+          key={account.account_id}
+          className="flex justify-between items-center mt-2 p-4 bg-white hover:border-blue-500 rounded border-2 border-gray-300 cursor-pointer">
           <div>
             <span>{account.official_name}</span>
-            <span class="text-gray-500">*{account.mask}</span>
-            <div class="text-gray-500 capitalize">{account.subtype}</div>
+            <span className="text-gray-500">*{account.mask}</span>
+            <div className="text-gray-500 capitalize">{account.subtype}</div>
           </div>
-          <div class="text-xl">
-            {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(account.balances.current)}
+          <div className="text-xl">
+            {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
+              account.balances.current,
+            )}
           </div>
         </div>
-      )}
+      ))}
     </div>
   );
 }
