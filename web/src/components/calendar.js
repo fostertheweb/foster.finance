81a7b47904d9466bb96b1749e929525cc72757dc
Jonathan Foster <jxf9099@rit.edu>
2020-02-29 10:16:43 -0500

    show transaction amounts on days

--- web/src/components/calendar.js
+++ web/src/components/calendar.js
@@ -1,6 +1,125 @@
 import React from "react";
 import * as luxon from "luxon";
 import classNames from "classnames";
+import Loading from "./loading";
+
+function Day({ number, isInCurrentMonth, isToday, data }) {
+  const day = ["box-border border-gray-400 border-r border-b flex-1 relative"];
+
+  return (
+    <div
+      className={classNames(
+        ...day,
+        isInCurrentMonth ? "bg-white" : "bg-gray-200",
+        isToday ? "bg-purple-100 text-purple-800 font-bold" : "",
+      )}>
+      <span className="float-right m-1">{number}</span>
+      {data ? (
+        <ul className="list-none m-2 float-left text-xs font-medium">
+          {data.map(t => (
+            <li key={t.transaction_id} className="mb-1">
+              {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
+                t.amount,
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+function Week({ days }) {
+  return <div className="flex flex-1">{days}</div>;
+}
+
+export function Calendar({ year, month, loading, data }) {
+  let weeks = [];
+  let key = 0;
+  let number = 1;
+  let maxRows = 5;
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (getFirstWeekday(year, month) === 7) {
+    maxRows = 6;
+  }
+
+  for (let row = 0; row < maxRows; row++) {
+    let week = [];
+
+    for (let weekday = 1; weekday < 8; weekday++) {
+      if (row === 0 && weekday === 6 && weekday === getFirstWeekday(year, month)) {
+        const calendarDate = luxon.DateTime.local(year, month, number);
+        const transactions = data ? data.filter(t => compareDates(t.date, calendarDate)) : [];
+        week.push(
+          <Day
+            number={number}
+            isInCurrentMonth={true}
+            isToday={isToday(year, month, number)}
+            data={transactions}
+            key={key}
+          />,
+        );
+        number++;
+        key++;
+      } else if (row === 0 && weekday === 7 && weekday === getFirstWeekday(year, month)) {
+        const calendarDate = luxon.DateTime.local(year, month, number);
+        const transactions = data ? data.filter(t => compareDates(t.date, calendarDate)) : [];
+        week.push(
+          <Day
+            number={number}
+            isInCurrentMonth={true}
+            isToday={isToday(year, month, number)}
+            data={transactions}
+            key={key}
+          />,
+        );
+        number++;
+        key++;
+      } else if (row === 0 && weekday < getFirstWeekday(year, month)) {
+        week.push(<Day number={""} isInCurrentMonth={false} isToday={false} key={key} />);
+        key++;
+      } else if (number > getDaysInMonth(year, month)) {
+        week.push(<Day number={""} isInCurrentMonth={false} isToday={false} key={key} />);
+        key++;
+      } else {
+        const calendarDate = luxon.DateTime.local(year, month, number);
+        const transactions = data ? data.filter(t => compareDates(t.date, calendarDate)) : [];
+        week.push(
+          <Day
+            number={number}
+            isInCurrentMonth={true}
+            isToday={isToday(year, month, number)}
+            data={transactions}
+            key={key}
+          />,
+        );
+        number++;
+        key++;
+      }
+    }
+
+    weeks.push(<Week days={week} key={row} />);
+  }
+
+  return (
+    <div className="flex flex-col border-l border-gray-400 calendar">
+      <div className="flex justify-around border-b border-gray-400 bg-white text-gray-700 weekdays">
+        <div>Sunday</div>
+        <div>Monday</div>
+        <div>Tuesday</div>
+        <div>Wednesday</div>
+        <div>Thursday</div>
+        <div>Friday</div>
+        <div>Saturday</div>
+      </div>
+      {weeks}
+    </div>
+  );
+}
 
 function getCurrentMonth() {
   return luxon.DateTime.local().month;
@@ -33,95 +152,6 @@ function isToday(year, month, day) {
   );
 }
 
-function Day({ number, isInCurrentMonth, isToday }) {
-  const day = ["box-border border-gray-400 border-r border-b flex-1 relative"];
-  return (
-    <div
-      className={classNames(
-        ...day,
-        isInCurrentMonth ? "bg-white" : "bg-gray-200",
-        isToday ? "bg-purple-100 text-purple-800 font-bold" : "",
-      )}>
-      <span className="float-right m-1">{number}</span>
-    </div>
-  );
-}
-
-function Week({ days }) {
-  return <div className="flex flex-1">{days}</div>;
-}
-
-export function Calendar({ year, month, loading, data }) {
-  let weeks = [];
-  let key = 0;
-  let number = 1;
-  let maxRows = 5;
-
-  if (getFirstWeekday(year, month) === 7) {
-    maxRows = 6;
-  }
-
-  for (let row = 0; row < maxRows; row++) {
-    let week = [];
-
-    for (let weekday = 1; weekday < 8; weekday++) {
-      if (row === 0 && weekday === 6 && weekday === getFirstWeekday(year, month)) {
-        week.push(
-          <Day
-            number={number}
-            isInCurrentMonth={true}
-            isToday={isToday(year, month, number)}
-            key={key}
-          />,
-        );
-        number++;
-        key++;
-      } else if (row === 0 && weekday === 7 && weekday === getFirstWeekday(year, month)) {
-        week.push(
-          <Day
-            number={number}
-            isInCurrentMonth={true}
-            isToday={isToday(year, month, number)}
-            key={key}
-          />,
-        );
-        number++;
-        key++;
-      } else if (row === 0 && weekday < getFirstWeekday(year, month)) {
-        week.push(<Day number={""} isInCurrentMonth={false} isToday={false} key={key} />);
-        key++;
-      } else if (number > getDaysInMonth(year, month)) {
-        week.push(<Day number={""} isInCurrentMonth={false} isToday={false} key={key} />);
-        key++;
-      } else {
-        week.push(
-          <Day
-            number={number}
-            isInCurrentMonth={true}
-            isToday={isToday(year, month, number)}
-            key={key}
-          />,
-        );
-        number++;
-        key++;
-      }
-    }
-
-    weeks.push(<Week days={week} key={row} />);
-  }
-
-  return (
-    <div className="flex flex-col border-l border-gray-400 calendar">
-      <div className="flex justify-around border-b border-gray-400 bg-white text-gray-700 weekdays">
-        <div>Sunday</div>
-        <div>Monday</div>
-        <div>Tuesday</div>
-        <div>Wednesday</div>
-        <div>Thursday</div>
-        <div>Friday</div>
-        <div>Saturday</div>
-      </div>
-      {weeks}
-    </div>
-  );
+function compareDates(transactionDate, calendarDate) {
+  return transactionDate === calendarDate.toFormat("yyyy-MM-dd");
 }
