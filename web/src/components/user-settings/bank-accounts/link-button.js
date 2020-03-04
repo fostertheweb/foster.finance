bbe3feb4414e58ae8c859dffa476de0fb8b585ea
Jonathan Foster <jxf9099@rit.edu>
2020-03-04 16:28:43 -0500

    stringify

--- web/src/components/user-settings/bank-accounts/link-button.js
+++ web/src/components/user-settings/bank-accounts/link-button.js
@@ -12,32 +12,38 @@ const publicKey = process.env.REACT_APP_PLAID_PUBLIC_KEY;
 export default function() {
   const { user } = useAuth();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function handleSuccess(token) {
     setLoading(true);
-    const uid = user.attributes.sub;
-    const items = JSON.parse(localStorage.getItem(uid)) || [];
-    const response = await fetch(`${url}/plaid/exchange`, {
-      method: "POST",
-      body: { public_token: token },
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const item = await response.json();
-    const { item_id, access_token } = item;
-    localStorage.setItem(
-      uid,
-      JSON.stringify([
-        ...items,
-        {
-          item_id,
-          access_token,
-          public_token: token,
+
+    try {
+      const uid = user.attributes.sub;
+      const items = JSON.parse(localStorage.getItem(uid)) || [];
+      const response = await fetch(`${url}/plaid/exchange`, {
+        method: "POST",
+        body: JSON.stringify({ public_token: token }),
+        headers: {
+          "Content-Type": "application/json",
         },
-      ]),
-    );
-    setLoading(false);
+      });
+      const { item_id, access_token } = await response.json();
+      localStorage.setItem(
+        uid,
+        JSON.stringify([
+          ...items,
+          {
+            item_id,
+            access_token,
+            public_token: token,
+          },
+        ]),
+      );
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -56,7 +62,7 @@ export default function() {
         size="lg"
         color="#666666"
       />
-      <span className="ml-2 font-medium">Connect to Bank</span>
+      <span className="ml-2 font-medium">{error ? error.message : "Connect to Bank"}</span>
     </PlaidLink>
   );
 }
