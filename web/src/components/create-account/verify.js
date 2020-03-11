49182fb4f9b1699beb13087b53b5f7dd4660b29e
Jonathan Foster <jxf9099@rit.edu>
2020-03-11 10:23:17 -0400

    better verify

--- web/src/components/create-account/verify.js
+++ web/src/components/create-account/verify.js
@@ -1,33 +1,39 @@
 import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane, faEnvelopeOpenText, faUserCheck } from "@fortawesome/pro-duotone-svg-icons";
+import {
+  faLongArrowAltRight,
+  faPaperPlane,
+  faEnvelopeOpenText,
+  faUserCheck,
+} from "@fortawesome/pro-duotone-svg-icons";
 import { parse } from "query-string";
 import { useAuth } from "../../hooks/use-auth";
 import Button from "../button";
 import { useLocation, useNavigate } from "react-router-dom";
 import Loading from "../loading";
 import Error from "../error";
+import Logo from "../logo";
 
 function CheckEmail({ email }) {
   const { resendSignUp, loading } = useAuth();
 
   return (
     <>
-      <div className="mx-4">
-        <FontAwesomeIcon icon={faEnvelopeOpenText} size="6x" color="#5f6c7b" />
+      <div className="w-full text-center mt-6">
+        <FontAwesomeIcon icon={faEnvelopeOpenText} size="5x" color="#5f6c7b" />
       </div>
-      <div className="lg:w-1/3 md:w-1/2 w-3/4 mx-4">
-        <h2>Email verification required</h2>
-        <p className="my-4">
-          You should have received an email from us with a link to verify your email address.
-        </p>
-        <Button
-          loading={loading}
-          text="Send the email again"
-          icon={faPaperPlane}
-          onClick={() => resendSignUp(email)}
-        />
-      </div>
+      <h2 className="mt-6">Email verification required</h2>
+      <p className="mt-2">
+        You should have received an email from us with a link to verify your email address.
+      </p>
+      <Button
+        large
+        className="w-full mt-6"
+        loading={loading}
+        text="Send email again"
+        icon={faPaperPlane}
+        onClick={() => resendSignUp(email)}
+      />
     </>
   );
 }
@@ -39,15 +45,12 @@ function Confirmed({ id, code }) {
 
   useEffect(() => {
     confirmSignUp(email, code);
-    setTimeout(() => {
-      navigate("/signin");
-    }, 1000);
     // eslint-disable-next-line
   }, []);
 
   if (loading) return <Loading />;
   if (error && error.code === "NotAuthorizedException") {
-    navigate("/signin");
+    navigate("/");
   }
   if (error) return <Error message={error.message} />;
 
@@ -56,10 +59,8 @@ function Confirmed({ id, code }) {
       <div className="mx-4">
         <FontAwesomeIcon icon={faUserCheck} size="5x" color="#5f6c7b" />
       </div>
-      <div className="lg:w-1/3 md:w-1/2 w-3/4 mx-4">
-        <h2>Email Verified</h2>
-        <p className="my-4">Wow thank you.</p>
-      </div>
+      <h2>Email Verified</h2>
+      <p className="my-4">Wow thank you.</p>
     </>
   );
 }
@@ -69,8 +70,26 @@ export default function() {
   const { id, code, email } = parse(location.search);
 
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center my-8">
-      {id ? <Confirmed id={id} code={code} /> : <CheckEmail email={email} />}
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex items-stretch w-1/2 bg-white rounded shadow">
+        <div className="p-6 border-r border-gray-200 w-1/2">
+          <div className="flex items-center justify-start">
+            <Logo />
+            <FontAwesomeIcon
+              icon={faLongArrowAltRight}
+              size="lg"
+              className="text-gray-400 fill-current ml-2 -mb-2"
+            />
+            <h2 className="text-gray-500 tracking-wide font-normal smallcaps text-xl ml-2 -mb-1">
+              Verify Email
+            </h2>
+          </div>
+          {id ? <Confirmed id={id} code={code} /> : <CheckEmail email={email} />}
+        </div>
+        <div className="w-1/2 rounded-r bg-green-100 text-green-500 flex items-center justify-center">
+          <p className="block p-4">poggers</p>
+        </div>
+      </div>
     </div>
   );
 }
