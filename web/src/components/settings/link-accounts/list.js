13030e0a6fd04e0474f835f9ecdb1bca53564132
Jonathan Foster <jxf9099@rit.edu>
2020-03-15 11:13:53 -0400

    gonna save accounts soon

--- web/src/components/settings/link-accounts/list.js
+++ web/src/components/settings/link-accounts/list.js
@@ -1,8 +1,9 @@
 import React from "react";
 import LinkButton from "./link-button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinnerThird, faLandmark, faCheck } from "@fortawesome/pro-duotone-svg-icons";
+import { faSpinnerThird, faLandmark, faCheck, faSave } from "@fortawesome/pro-duotone-svg-icons";
 import Alert from "../../alert";
+import Button from "../../button";
 
 export default function({ loading, error, data }) {
   return (
@@ -29,34 +30,46 @@ export default function({ loading, error, data }) {
           {error ? (
             <Alert error={error.message || error} />
           ) : (
-            <div className="my-2 border border-gray-300 rounded">
-              {data.map(account => (
-                <div
-                  key={account.account_id}
-                  className="flex items-center border-t first:border-t-0 border-gray-300 p-2 bg-white hover:bg-gray-100 rounded-sm cursor-pointer">
-                  <div className="w-10 text-center mr-2">
-                    {account.selected ? (
-                      <FontAwesomeIcon icon={faCheck} size="lg" color={"#3da9fc"} />
-                    ) : null}
-                  </div>
-                  <div className="flex-grow">
-                    <span className="mr-2">{account.official_name || account.name}</span>
-                    <span className="text-gray-500">*{account.mask}</span>
-                    <div
-                      className={`mt-1 text-gray-500 ${
-                        account.subtype.length <= 3 ? "uppercase" : "capitalize"
-                      }`}>
-                      {account.subtype}
+            <>
+              <div className="my-2 border border-gray-300 rounded">
+                {data.map(account => (
+                  <div
+                    key={account.account_id}
+                    className="flex items-center border-t first:border-t-0 border-gray-300 p-2 bg-white hover:bg-gray-100 rounded-sm cursor-pointer">
+                    <div className="w-10 text-center mr-2">
+                      {account.selected ? (
+                        <FontAwesomeIcon icon={faCheck} size="lg" color={"#3da9fc"} />
+                      ) : null}
+                    </div>
+                    <div className="flex-grow">
+                      <span className="mr-2">{account.official_name || account.name}</span>
+                      <span className="text-gray-500">*{account.mask}</span>
+                      <div
+                        className={`mt-1 text-gray-500 ${
+                          account.subtype.length <= 3 ? "uppercase" : "capitalize"
+                        }`}>
+                        {account.subtype}
+                      </div>
+                    </div>
+                    <div className="text-md">
+                      {new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                      }).format(account.balances.current)}
                     </div>
                   </div>
-                  <div className="text-md">
-                    {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(
-                      account.balances.current,
-                    )}
-                  </div>
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>
+              <div className="my-4 flex justify-end">
+                <Button
+                  text="Save Account Selection"
+                  icon={faSave}
+                  loading={false}
+                  disabled={loading}
+                  onClick={() => console.log(data.filter(a => a.selected).map(a => a.account_id))}
+                />
+              </div>
+            </>
           )}
         </>
       )}
