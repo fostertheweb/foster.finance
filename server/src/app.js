510d1bedd56188a235e6e231208aeab39ba372d2
Jonathan Foster <fostertheweb@gmail.com>
2021-03-11 14:47:51 -0500

    ok

--- server/src/app.js
+++ server/src/app.js
@@ -1,28 +1,23 @@
 const app = require("fastify")({ logger: true });
+const mercurius = require('mercurius');
+const Plaid = require("plaid");
+
+const plaid = new Plaid.Client({
+	clientID: process.env.PLAID_CLIENT_ID,
+	secret: process.env.PLAID_SECRET,
+	env: Plaid.environments[process.env.PLAID_ENV],
+	options: { version: "2019-05-29" },
+});
 
 // health check
 app.get("/ping", () => "PONG");
 
 // installed plugins
 app.register(require("fastify-sensible"));
-app.register(require("fastify-jwt"), { secret: "chipwheel" });
-app.register(require("fastify-mongodb"), { url: process.env.DB_URL });
-app.register(require("fastify-cors"), {
-	origin: true,
-	methods: ["GET", "POST", "PUT", "PATCH"],
-	allowedHeaders: ["Content-Type", "Authorization", "X-Amz-User-Agent", "X-Amz-Security-Token", "X-Amz-Date"],
-	credentials: true,
-	maxAage: 300,
-});
-
-// plugins
-app.register(require("./plugins/plaid"));
-// app.register(require("./plugins/session"));
 
 // routes
 app.register(require("./routes/accounts"), { prefix: "/accounts" });
 app.register(require("./routes/expenses"), { prefix: "/expenses" });
-app.register(require("./routes/profile"), { prefix: "/profile" });
 app.register(require("./routes/transactions"), { prefix: "/transactions" });
 
 module.exports = app;
