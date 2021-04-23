10b11a5b1f6a2711914f8f7d4ef5a9fa46da460e
Jonathan Foster <fostertheweb@gmail.com>
2021-04-23 11:42:03 -0400

    update api tf

--- server/main.tf
+++ server/main.tf
@@ -101,12 +101,30 @@ data "aws_route53_zone" "selected" {
   name = "${var.domain_name}."
 }
 
-resource "aws_route53_record" "api" {
-  zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "api.${var.domain_name}"
-  type    = "A"
-  records = ["${aws_apigatewayv2_api.server.api_endpoint}/foster-finance-server"]
-	ttl     = 300
+data "aws_acm_certificate" "issued" {
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+	most_recent = true
+}
 
-  depends_on = [aws_apigatewayv2_api.server]
+resource "aws_apigatewayv2_domain_name" "api" {
+  domain_name = "api.${var.domain_name}"
+
+  domain_name_configuration {
+    certificate_arn = data.aws_acm_certificate.issued.arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+}
+
+resource "aws_route53_record" "api" {
+  name    = aws_apigatewayv2_domain_name.api.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.selected.zone_id
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.api.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.api.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
 }
