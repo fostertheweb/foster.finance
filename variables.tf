71178a6c618e08ba7460a6ba740df0560610fd90
Jonathan Foster <fostertheweb@gmail.com>
2021-04-22 20:18:29 -0400

    oops

--- variables.tf
+++ variables.tf
@@ -7,19 +7,3 @@ variable "domain_name" {
   default = "foster.finance"
   type    = string
 }
-
-variable "plaid_client_id" {
-  type = string
-}
-
-variable "plaid_public_key" {
-  type = string
-}
-
-variable "plaid_secret" {
-  type = string
-}
-
-variable "plaid_env" {
-  type = string
-}
